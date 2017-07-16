60c910d09208782f870f9ba1d2aba78aadb93bed
UltimaQ <ultimaq@localhost.localdomain>
2017-07-16 00:52:04 -0500

    modified:   society.c

--- society.c
+++ society.c
@@ -2,13 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define SELF 1
-#define NOSF 0;
+#define true 1
+#define false 0;
 
-person{
-  int style = NOSF;
+/************
+ * will be used to
+ * create and monitor
+ * the people of the society
+ ************/
+struct person{
+  int SELF = false;
   int happy = 0;
-}
+};
 
 int main (int nargs, char * args){
   int popl;
+  //the creation of a society takes a sec
+  while(k){
+    person new;
+    pop[k] = new;
+  }
+
+  return 0;
+}
