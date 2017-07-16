16c638f424dda13fe9aa518094ee9b6afd1a1044
Ultimaq <gcw66@protonmail.com>
2017-07-15 22:01:14 -0500

    modified:   society.c

---
+++ society.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SELF 1
+#define NOSF 0;
+
+person{
+  int style = NOSF;
+  int happy = 0;
+}
+
+int main (int nargs, char * args){
+  int popl;
